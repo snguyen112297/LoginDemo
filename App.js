2956c4d606783dd0fc549f58bafa12f6381245cc
snguyen112297 <snguyen@fandm.edu>
2020-09-03 12:54:23 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
+export default function App(){
+  const [username, setUsername] = useState("username")
+  const [password, setPassword] = useState("password")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput style={styles.textinput} placeholderTextColor="#ffffff" onChangeText={text => setUsername(text)}>{username}</TextInput>
+      </View>
+      <View style={styles.inputView}>
+    <TextInput style={styles.textinput} placeholderTextColor="#ffffff" onChangeText={text => setPassword(text)}>{password}</TextInput>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.loginBtn} onPress={
+          () => {setUsername(""), setPassword("")}}>
+
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -18,4 +31,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    color: "green"
+  },
+  textinput: {
+    color: "white",
+    height: 50
+  },
+  inputView:{
+    width:"70%",
+    backgroundColor:"black",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  loginBtn:{
+    width:"70%",
+    backgroundColor:"black",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:20,
+    padding: 20
+  },
+  loginText: {
+    color: "white"
+  }
 });
---
+++ README.md
@@ -0,0 +1,10 @@
+Screenshots
+
+Input username + password
+
+[![Screenshot-1599151908.png](https://i.postimg.cc/1zqYV7kM/Screenshot-1599151908.png)](https://postimg.cc/B8JCdpQ1)
+
+
+Press login
+
+[![Screenshot-1599151916.png](https://i.postimg.cc/jqX3f71V/Screenshot-1599151916.png)](https://postimg.cc/z3ykZV5p)
